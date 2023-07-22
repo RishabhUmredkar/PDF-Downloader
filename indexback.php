c467888e0a41bd54cf8173ad63131c15dbf3cf9d
Rishabh Umredkar <umredkarrishabh@gmail.com>
2023-07-22 23:59:57 +0530

    first commit

---
+++ indexback.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Download PDF using PHP from HTML link</title>
+</head>
+<body>
+  <center>
+    <h2 style  = "color :red;">Welcome to Asterisc Technocrat </h2>
+    <p><b>Click below to Download PDF</b></p>
+    <a href="download.php?file=java_tutorial(internship)" target="_blank">Download PDF Now</a>
+    
+</center>  
+</body>
+</html>
